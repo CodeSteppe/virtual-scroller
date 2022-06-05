b85145ef6b60db04de1427854a9e2b129253fbb8
MudOnTire <895157882@qq.com>
2022-06-05 13:55:10 +0800

    update

--- VirtualScroller.js
+++ VirtualScroller.js
@@ -65,6 +65,13 @@ class VirtualScroller {
     this.scroller.addEventListener('scroll', debounce(this.#handleScroll, 200));
   }
 
+  #topHiddenCount = 0;
+  #bottomHiddenCount = 0;
+  #scrollTop = 0;
+  #paddingTop = 0;
+  #paddingBottom = 0;
+  #lastVisibleItemIndex = 0;
+
   #loadInitData() {
     const scrollerRect = this.scroller.getBoundingClientRect();
     const minCount = Math.ceil(scrollerRect.height / this.rowHeight);
@@ -74,22 +81,83 @@ class VirtualScroller {
     this.#renderNewData(newData);
   }
 
+  #renderRow(item) {
+    const rowContent = this.renderItem(item);
+    const row = document.createElement('div');
+    row.dataset.index = item
+    row.style.height = this.rowHeight + 'px';
+    row.appendChild(rowContent)
+    return row;
+  }
+
   #renderNewData(newData) {
     newData.forEach(item => {
-      const rowContent = this.renderItem(item);
-      const row = document.createElement('div');
-      row.style.height = this.rowHeight + 'px';
-      row.appendChild(rowContent)
-      this.contentBox.append(row);
+      this.contentBox.append(this.#renderRow(item));
     });
   }
 
   #handleScroll = (e) => {
     const { clientHeight, scrollHeight, scrollTop } = e.target;
     if (scrollHeight - (clientHeight + scrollTop) < 40) {
+      console.log('load more');
       const newData = this.loadMore(this.pageSize);
       this.data.push(...newData);
-      this.#renderNewData(newData);
     }
+    const direction = scrollTop > this.#scrollTop ? 1 : -1;
+    this.#toggleTopItems(direction);
+    this.#toggleBottomItems(direction);
+    this.#scrollTop = scrollTop;
+    console.log({
+      direction,
+      topHiddenCount: this.#topHiddenCount,
+      lastVisibleItemIndex: this.#lastVisibleItemIndex
+    });
+  }
+
+  #toggleTopItems = (direction) => {
+    const { scrollTop } = this.scroller;
+    const firstVisibleItemIndex = Math.floor(scrollTop / this.rowHeight);
+    const rows = this.contentBox.children;
+    // replace invisible top items with padding top
+    if (direction === 1) {
+      for (let i = this.#topHiddenCount; i < firstVisibleItemIndex; i++) {
+        if (rows[0]) rows[0].remove();
+      }
+    }
+    // restore hidden top items
+    if (direction === -1) {
+      for (let i = this.#topHiddenCount - 1; i >= firstVisibleItemIndex; i--) {
+        const item = this.data[i];
+        const row = this.#renderRow(item);
+        this.contentBox.prepend(row);
+      }
+    }
+    this.#topHiddenCount = firstVisibleItemIndex;
+    this.#paddingTop = this.#topHiddenCount * this.rowHeight;
+    this.contentBox.style.paddingTop = this.#paddingTop + 'px';
+  }
+
+  #toggleBottomItems = (direction) => {
+    const { scrollTop, clientHeight } = this.scroller;
+    const lastVisibleItemIndex = Math.floor((scrollTop + clientHeight) / this.rowHeight);
+    this.#lastVisibleItemIndex = lastVisibleItemIndex;
+    const rows = [...this.contentBox.children];
+    // replace invisible bottom items with padding bottom
+    if (direction === -1) {
+      for (let i = lastVisibleItemIndex + 1; i <= this.data.length; i++) {
+        const row = rows[i - this.#topHiddenCount];
+        if (row) row.remove();
+      }
+    }
+    // restore hidden bottom items
+    if (direction === 1) {
+      for (let i = this.#topHiddenCount + rows.length; i <= lastVisibleItemIndex; i++) {
+        const item = this.data[i];
+        const row = this.#renderRow(item);
+        this.contentBox.append(row);
+      }
+    }
+    this.#bottomHiddenCount = this.data.length - (this.#topHiddenCount + this.contentBox.children.length);
+    this.contentBox.style.paddingBottom = this.#bottomHiddenCount * this.rowHeight + 'px';
   }
 }
--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const virtualScroller = new VirtualScroller({
   element: '#virtual-scroller',
   height: '80vh',
   rowHeight: 60, // px
-  pageSize: 100,
+  pageSize: 20,
+  // buffer: 10,
   renderItem: function (dataItem) {
     const div = document.createElement('div');
     div.classList.add('row-content');
